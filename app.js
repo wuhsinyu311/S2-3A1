2d5361c79aa880c7cbf2fba6e5c024546cf49dd0
wuhsinyu311 <130282695+wuhsinyu311@users.noreply.github.com>
2023-04-30 16:13:06 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require handlebars in the project
+const exphbs =  require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set ('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting    
+  app.get('/', (req, res) => {
+    res.render('index', { restaurants:restaurantList.results })
+  })
+  
+  app.get('/restaurants/:restaurant_id', (req, res) => {  
+    const restaurant = restaurantList.results.find(
+      restaurant => restaurant.id.toString() === req.params.restaurant_id
+    )  
+    res.render('show', { restaurant: restaurant })
+  })
+
+  app.get('/search', (req, res) => {
+    const keyword = req.query.keyword.trim().toLowerCase()
+    const filteredRestaurants = restaurantList.results.filter(
+        restaurant => restaurant.name.toLowerCase().includes(keyword) ||
+        restaurant.category.toLowerCase().includes(keyword)
+      )
+    res.render('index', { restaurants: filteredRestaurants, keyword: keyword })
+  })
+
+// start and listen on the Express server
+app.listen(port, () =>{
+    console.log(`Express is listening on http://localhost:${port}`)
+})
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Restaurant List
+用Node.js和express完成的專案，列出餐廳清單供使用者查詢並瀏覽餐廳資訊。
+![image](https://raw.githubusercontent.com/wuhsinyu/S2-3A1/main/public/image/restaurant_list_image.png)
+
+## Features
+- 查看所有用restaurant api串接的餐廳
+- 用名稱或餐廳類別搜尋特定餐廳
+- 顯示餐廳的詳細資料
+
+## Prerequisites
+- Node.js
+- Express @4.16.4
+- Express-handlebars @3.0.0
+
+### Installing
+1.請先確認已安裝node.js和npm
+
+2.clone專案到本機
+
+3.安裝 express
+```
+npm i express@4.16.4
+```
+
+4.安裝 handlebars
+```
+npm i express-handlebars@3.0.0
+```
+
+5.安裝nodemon 
+```
+npm install -g nodemon
+```
+    
